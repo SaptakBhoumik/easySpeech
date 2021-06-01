c78366a113409b84f56cc2dd167900147f73f13a
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-06-01 12:55:44 +0530

    Add files via upload

--- easySpeech/__init__.py
+++ easySpeech/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 
 __project__      = 'easySpeech'
-__version__      = '0.0.1'
+__version__      = '1'
 __keywords__     = ['easySpeech', 'easyspeech', 'speech', 'speech_recognition']
 __author__       = 'Saptak Bhoumik'
 __author_email__ = 'saptakbhoumik@gmail.com'
@@ -10,7 +10,7 @@ __url__          = 'https://github.com/SaptakBhoumik/easySpeech'
 __platforms__    = 'ALL'
 
 __classifiers__ = [
-    "Topic :: Utilities",
+    "Topic :: Multimedia :: Sound/Audio :: Speech",
     "License :: OSI Approved :: MIT License",
     "Programming Language :: Python :: 3",
 ]
